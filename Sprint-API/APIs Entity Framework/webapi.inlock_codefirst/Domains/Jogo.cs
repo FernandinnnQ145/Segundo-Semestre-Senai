e167c226cecdb352f9341b4b7520361e0cbf44b3
Fernando Quatrochi <fernandinnn145@gmail.com>
2023-09-13 17:24:12 -0300

    Fazendo API Inlock Entity Fremework

---
+++ Sprint-API/APIs Entity Framework/webapi.inlock_codefirst/Domains/Jogo.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.inlock_codefirst.Domains
+{
+
+    [Table("Jogo")]
+    public class Jogo
+    {
+        [Key]
+        public Guid IdJogo { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "VARCHAR(100)")]
+        [Required(ErrorMessage = "Nome do jogo obrigatorio")]
+        public string Nome { get; set; }
+
+        [Column(TypeName = "TEXT")]
+        [Required(ErrorMessage = "Descricao do jogo obrigatoria")]
+        public string Descricao { get; set; }
+
+        [Column(TypeName = "DATE")]
+        [Required(ErrorMessage = "Data de lancamento jogo obrigatoria")]
+        public DateTime DataLacamento { get; set; }
+
+        [Column(TypeName = "Decimal(4,2)")]
+        [Required(ErrorMessage = "preco do jogo obrigatorio")]
+        public decimal Preco { get; set; }
+
+        //ref.tabela estudio - FK
+        
+        public Guid IdEstudio { get; set; }
+
+        [ForeignKey("IdEstudio")]
+        public Estudio Estudio { get; set; }
+    }
+}
