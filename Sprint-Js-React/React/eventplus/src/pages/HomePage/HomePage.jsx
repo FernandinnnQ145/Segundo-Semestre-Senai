9442a3c848d73775c6a34fc73c7d319174a68337
Fernando Quatrochi <fernandinnn145@gmail.com>
2023-11-09 17:25:00 -0300

    Fazendo projeto

--- Sprint-Js-React/React/eventplus/src/pages/HomePage/HomePage.jsx
+++ Sprint-Js-React/React/eventplus/src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './HomePage.css'
 import MainContent from '../../Components/MainContent/MainContent';
 import Banner from '../../Components/Banner/Banner';
@@ -8,7 +8,24 @@ import NextEvent from '../../Components/NextEvent/NextEvent';
 import Container from '../../Components/Container/Container';
 import Title from '../../Components/Title/Title';
 const HomePage = () => {
+    useState(() =>{
+        //Chamar a api
+        
+    }, [])
+
+    //Fake mock - api mocada
+    const [nextEvent, setNextEvent] = useState([
+        {id: 1, title: "EventoX", descricao: "Evento de Sql", data:"10/11/2023" },
+        {id: 2, title: "EventoY", descricao: "Evento de C#", data:"19/11/2023" }
+    ])
+
+
+
     return (
+        
+
+
+
         <MainContent>
             <Banner />
 
@@ -20,26 +37,18 @@ const HomePage = () => {
                     <div className="events-box">
 
 
-                        <NextEvent
-                            title={"Evento x"}
-                            description={"Evento legal"}
-                            eventDate={"14/03/2024"}
-                            idEvento={"dfvbrfedw"} />
-                        <NextEvent
-                            title={"Evento x"}
-                            description={"Evento legal"}
-                            eventDate={"14/03/2024"}
-                            idEvento={"dfvbrfedw"} />
-                        <NextEvent
-                            title={"Evento x"}
-                            description={"Evento legal"}
-                            eventDate={"14/03/2024"}
-                            idEvento={"dfvbrfedw"} />
-                        <NextEvent
-                            title={"Evento x"}
-                            description={"Evento legalaaaaaaaaaaaa aaaaaaaaaa aaaaaaaa"}
-                            eventDate={"14/03/2024"}
-                            idEvento={"dfvbrfedw"} />
+                        
+                            {nextEvent.map((e) => {
+                                return(
+                                    <NextEvent
+                                    title={e.title}
+                                    description={e.descricao}
+                                    eventDate={e.data}
+                                    idEvento={e.id} />
+                                )
+                            })}
+
+                          
                         
                        
 
--- Sprint-Js-React/React/eventplus/src/pages/TestePage/TestePage.jsx
+++ Sprint-Js-React/React/eventplus/src/pages/TestePage/TestePage.jsx
@@ -1,47 +1,19 @@
-import React, { useState } from "react";
-import Input from "../../Components/Input/Input";
-import Button from "../../Components/Button/Button";
-import Header from "../../Components/Header/Header";
+import React, {useEffect, useState} from 'react';
+import './TestePage.css'
 
 const TestePage = () => {
+  const [count, setCount] = useState(10);
+  const [calculation, setCalculation] = useState(10);
 
-  const [total, setTotal] = useState();
-  const [n1, setN1] = useState();
-  const [n2, setN2] = useState();
-  
-  function handleCalcular(e) {//chamar no submit do form
-    e.preventDefault();
-    setTotal( parseFloat(n1) + parseFloat(n2) );
-  }
+  useEffect(() =>{
+    setCalculation(count * 2)
+  }, [])
 
   return (
     <>
-      <Header />
-      <h1>Página de Testes</h1>
-      <h2>Calculator</h2>
-      <p>Resultado: <strong>{total}</strong></p>
-
-      <form onSubmit={handleCalcular}>
-        <Input 
-          tipo="number"
-          id="numero1"
-          nome="numero1"
-          dicaCampo="Primeiro Número"
-          valor={n1}
-          fnAltera={setN1}
-          />
-        <Input
-          tipo="number"
-          id="numero2"
-          nome="numero2"
-          dicaCampo="Segundo Número"
-          valor={n2}
-          fnAltera={setN2}
-        />
-
-        <Button tipo="submit" textoBotao="Somar" />
-        <p>Resultado: <strong>{total}</strong></p>
-      </form>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <p>Calculation: {calculation}</p>
     </>
   );
 };
